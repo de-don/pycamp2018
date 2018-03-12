adf5463c01a14f25b38de7528055bf40a1ada976
DenisDontsov <denis.dontsov@saritasa.com>
2018-03-12 14:04:46 +0700

    Added tests

--- week_1/day_3/problem_0_tests.py
+++ week_1/day_3/problem_0_tests.py
@@ -1,22 +1,26 @@
 from unittest import TestCase
 
-from .problem_0 import translit_text
+from .problem_0 import transliteration
 
 
 class Problem0Tests(TestCase):
+    def setUp(self):
+        self.rus2eng, self.eng2rus = transliteration()
 
     def test_one_word(self):
-        self.assertEqual(translit_text("Привет"), "Privet")
-        self.assertEqual(translit_text("Денис"), "Denis")
-        self.assertEqual(translit_text("интересно"), "interesno")
-        self.assertEqual(translit_text("есть"), "yest")
-        self.assertEqual(translit_text("Есть"), "Yest")
+        rus = "Привет"
+        eng = self.rus2eng(rus)
+        self.assertEqual(eng, "Privet")
+        self.assertEqual(self.eng2rus(eng), rus)
 
     def test_punctuation(self):
-        self.assertEqual(translit_text("Привет, Юзер!"), "Privet, Yuzer!")
-        self.assertEqual(translit_text("!Привет, Юзер!"), "!Privet, Yuzer!")
+        rus = "!ЗдравсТвуй, Юзер!"
+        eng = self.rus2eng(rus)
+        self.assertEqual(eng, "!ZdravsTvujj, Juzer!")
+        self.assertEqual(self.eng2rus(eng), rus)
 
-    def test_pairs(self):
-        self.assertEqual(translit_text("не нее"), "ne neye")
-        self.assertEqual(translit_text("пьяный пьяный"), "pianiy pianiy")
-        self.assertEqual(translit_text("тощий тощий"), "toshchy toshchy")
+    def test_complex(self):
+        rus = "не нее, льняной, тощий"
+        eng = self.rus2eng(rus)
+        self.assertEqual(eng, "ne nee, l'njanojj, toshhijj")
+        self.assertEqual(self.eng2rus(eng), rus)
