3e5e34860777b38ca5dfb37cffc3d9556cbc708e
DenisDontsov <denis.dontsov@saritasa.com>
2018-03-12 14:03:46 +0700

    The change is a character-by-character test for the common replacement in a string. Added reverse transliteration.

--- week_1/day_3/problem_0.py
+++ week_1/day_3/problem_0.py
@@ -1,87 +1,76 @@
-import re
+def transliteration():
+    single_letters = {
+        "а": "a",
+        "б": "b",
+        "в": "v",
+        "г": "g",
+        "д": "d",
+        "е": "e",
+        "з": "z",
+        "и": "i",
+        "к": "k",
+        "л": "l",
+        "м": "m",
+        "н": "n",
+        "о": "o",
+        "п": "p",
+        "р": "r",
+        "с": "s",
+        "т": "t",
+        "у": "u",
+        "ф": "f",
+        "ц": "c",
+    }
 
-ru2eng = {
-    "а": "a",
-    "б": "b",
-    "в": "v",
-    "г": "g",
-    "д": "d",
-    "е": "e",
-    "ё": "e",
-    "ж": "zh",
-    "з": "z",
-    "и": "i",
-    "й": "y",
-    "к": "k",
-    "л": "l",
-    "м": "m",
-    "н": "n",
-    "о": "o",
-    "п": "p",
-    "р": "r",
-    "с": "s",
-    "т": "t",
-    "у": "u",
-    "ф": "f",
-    "х": "kh",
-    "ц": "ts",
-    "ч": "ch",
-    "ш": "sh",
-    "щ": "shch",
-    "ъ": "",
-    "ы": "",
-    "ь": "",
-    "э": "e",
-    "ю": "yu",
-    "я": "ya"
-}
-glasn = "ёуеаоэяию"
+    not_capitalize_letters = {
+        "ь": "'",
+        "ъ": "\"",
+        "ы": "y",
+    }
 
+    pairs_letters = {
+        'й': "jj",
+        "ё": "jo",
+        "ж": "zh",
+        "х": "kh",
+        "ч": "ch",
+        "ш": "sh",
+        "э": "eh",
+        "ю": "ju",
+        "я": "ja",
+    }
 
-def translit_word(word):
-    """ Translit single word by rules https://www.nic.ru/dns/translit.shtml """
+    triples_letters = {
+        "щ": "shh",
+    }
+    capitalize_single_letters = {k.upper(): v.upper() for k, v in single_letters.items()}
+    capitalize_pairs_letters = {k.capitalize(): v.capitalize() for k, v in pairs_letters.items()}
+    capitalize_triples_letters = {k.capitalize(): v.capitalize() for k, v in triples_letters.items()}
 
-    output_str = ""
+    def rus2eng(string):
+        """ Function to translit string"""
 
-    for i, ch in enumerate(word):
-        ch_lower = ch.lower()
-        to_ch = ru2eng.get(ch.lower(), None)
-        end = False
+        for dictionary in (capitalize_triples_letters, capitalize_pairs_letters, capitalize_single_letters):
+            for cyrillic_string, latin_string in dictionary.items():
+                string = string.replace(cyrillic_string, latin_string)
 
-        if ch_lower in "её":
-            if i == 0 or word[i - 1].lower() in glasn:
-                to_ch = "ye"
+        for dictionary in (triples_letters, pairs_letters, single_letters, not_capitalize_letters):
+            for cyrillic_string, latin_string in dictionary.items():
+                string = string.replace(cyrillic_string, latin_string)
 
-        if ch_lower == "я":
-            if i > 0 and word[i - 1].lower() == "ь":
-                to_ch = "ia"
+        return string
 
-        if i == len(word) - 2 and word[i + 1].lower() == "й":
-            if ch_lower == "и":
-                to_ch = "y"
-                end = True
-            if ch_lower == "ы":
-                to_ch = "iy"
-                end = True
+    def eng2rus(string):
+        """ Function to translit string"""
 
-        if to_ch is None:
-            to_ch = ch
-        if ch.isupper():
-            output_str += to_ch.capitalize()
-        else:
-            output_str += to_ch
+        for dictionary in (capitalize_triples_letters, capitalize_pairs_letters, capitalize_single_letters):
+            for cyrillic_string, latin_string in dictionary.items():
+                string = string.replace(latin_string, cyrillic_string)
 
-        if end:
-            break
+        for dictionary in (triples_letters, pairs_letters, single_letters, not_capitalize_letters):
+            for cyrillic_string, latin_string in dictionary.items():
+                string = string.replace(latin_string, cyrillic_string)
 
-    return output_str
+        return string
 
-
-def translit_text(text):
-    """ Function to translit text"""
-
-    # find all substrings: word or punstuation
-    substrings = re.findall(r"[-А-Яа-я]+|[^-А-Яа-я]+", text)
-
-    # translit all substrings and join it
-    return "".join(map(translit_word, substrings))
+    return rus2eng, eng2rus
