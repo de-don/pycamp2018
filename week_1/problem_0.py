d09747e22cf2e494e57baa2636d32bbef5612a59
DenisDontsov <denis.dontsov@saritasa.com>
2018-03-14 10:25:58 +0700

    Change algorithm to each-symbol editing

--- week_1/problem_0.py
+++ week_1/problem_0.py
@@ -1,109 +1,85 @@
-def transliteration():
-    """Function which create functions to transliteration
+RUS_TO_ENG = {
+    'а': 'a',
+    'б': 'b',
+    'в': 'v',
+    'г': 'g',
+    'д': 'd',
+    'е': 'e',
+    'з': 'z',
+    'и': 'i',
+    'к': 'k',
+    'л': 'l',
+    'м': 'm',
+    'н': 'n',
+    'о': 'o',
+    'п': 'p',
+    'р': 'r',
+    'с': 's',
+    'т': 't',
+    'у': 'u',
+    'ф': 'f',
+    'ц': 'c',
+    'ь': '\'',
+    'ъ': '\"',
+    'ы': 'y',
+    'й': 'jj',
+    'ё': 'jo',
+    'ж': 'zh',
+    'х': 'kh',
+    'ч': 'ch',
+    'ш': 'sh',
+    'э': 'eh',
+    'ю': 'ju',
+    'я': 'ja',
+    'щ': 'shh',
+}
+ENG_TO_RUS = dict(map(reversed, RUS_TO_ENG.items()))
+
+
+def transliteration(string, direction='rus2eng'):
+    """ Function to transliterate string.
+
+    Args:
+        string(str): string to transliterate.
+        direction(str): direction of transliterate. "eng2rus" or "rus2eng"
 
     Returns:
-        function: function to transliteration from russian to english
-        function: function to transliteration from english to russian
+        str: translited string
+
     """
 
-    single_letters = {
-        'а': 'a',
-        'б': 'b',
-        'в': 'v',
-        'г': 'g',
-        'д': 'd',
-        'е': 'e',
-        'з': 'z',
-        'и': 'i',
-        'к': 'k',
-        'л': 'l',
-        'м': 'm',
-        'н': 'n',
-        'о': 'o',
-        'п': 'p',
-        'р': 'r',
-        'с': 's',
-        'т': 't',
-        'у': 'u',
-        'ф': 'f',
-        'ц': 'c',
-    }
-    not_capitalize_letters = {
-        'ь': '\'',
-        'ъ': '\"',
-        'ы': 'y',
-    }
-    pairs_letters = {
-        'й': 'jj',
-        'ё': 'jo',
-        'ж': 'zh',
-        'х': 'kh',
-        'ч': 'ch',
-        'ш': 'sh',
-        'э': 'eh',
-        'ю': 'ju',
-        'я': 'ja',
-    }
-    triples_letters = {
-        'щ': 'shh',
-    }
+    if direction == 'rus2eng':
+        diction = RUS_TO_ENG
+    elif direction == 'eng2rus':
+        diction = ENG_TO_RUS
+    else:
+        raise ValueError(f'Direction "{direction}" is not supported')
 
-    # generate capitalized cases
-    capitalize_single_letters = {
-        k.upper(): v.upper() for k, v in single_letters.items()
-    }
-    capitalize_pairs_letters = {
-        k.capitalize(): v.capitalize() for k, v in pairs_letters.items()
-    }
-    capitalize_triples_letters = {
-        k.capitalize(): v.capitalize() for k, v in triples_letters.items()
-    }
+    # max replace length in diction
+    max_replace_len = max(map(len, diction.keys()))
 
-    def translite(string, rev=False):
-        """ Function to translit string from Russian to English and reverse.
+    position = 0
+    out_string = []
+    while position < len(string):
 
-        Args:
-            string(str): string to transliteration
-            rev(bool): translit from russian to english (False),
-                or from english to russian (True).
+        # check all substrings from longest to shortest
+        for substring_len in range(max_replace_len, 0, -1):
+            substring = string[position:position + substring_len].lower()
 
-        Returns:
-            string: translited string
-        """
+            # if replace is found, remember it and break loop
+            if substring in diction:
+                replace = diction[substring]
+                break
+        else:
+            # if replace not found, set symbol without changes
+            substring_len = 1
+            replace = string[position]
 
-        for dicts in (capitalize_triples_letters, capitalize_pairs_letters,
-                      capitalize_single_letters, triples_letters,
-                      pairs_letters, single_letters, not_capitalize_letters):
-            for cyrillic_string, latin_string in dicts.items():
-                if rev:
-                    string = string.replace(latin_string, cyrillic_string)
-                else:
-                    string = string.replace(cyrillic_string, latin_string)
+        # if current symbol is upper, capitalized all replace
+        if string[position].isupper():
+            replace = replace.capitalize()
 
-        return string
+        out_string.append(replace)
+        position += substring_len
 
-    def rus2eng(string):
-        """ Function to transliteration string from russian to english
-
-        Args:
-            string(str): string on russian
-
-        Returns:
-            string: string on english
-        """
-
-        return translite(string, False)
-
-    def eng2rus(string):
-        """ Function to to transliteration from english to russian
-
-        Args:
-            string(str): string on english
-
-        Returns:
-            string: string on russian
-        """
-
-        return translite(string, True)
-
-    return rus2eng, eng2rus
+    return ''.join(out_string)
--- week_1/problem_0_tests.py
+++ week_1/problem_0_tests.py
@@ -4,23 +4,24 @@ from .problem_0 import transliteration
 
 
 class Problem0Tests(TestCase):
-    def setUp(self):
-        self.rus2eng, self.eng2rus = transliteration()
-
     def test_one_word(self):
         rus = 'Привет'
-        eng = self.rus2eng(rus)
+        eng = transliteration(rus)
         self.assertEqual(eng, 'Privet')
-        self.assertEqual(self.eng2rus(eng), rus)
+        self.assertEqual(transliteration(eng, "eng2rus"), rus)
 
     def test_punctuation(self):
         rus = '!ЗдравсТвуй, Юзер!'
-        eng = self.rus2eng(rus)
+        eng = transliteration(rus)
         self.assertEqual(eng, '!ZdravsTvujj, Juzer!')
-        self.assertEqual(self.eng2rus(eng), rus)
+        self.assertEqual(transliteration(eng, "eng2rus"), rus)
 
     def test_complex(self):
         rus = 'не нее, льняной, тощий'
-        eng = self.rus2eng(rus)
+        eng = transliteration(rus)
         self.assertEqual(eng, 'ne nee, l\'njanojj, toshhijj')
-        self.assertEqual(self.eng2rus(eng), rus)
+        self.assertEqual(transliteration(eng, "eng2rus"), rus)
+
+    def test_undefined_direction(self):
+        with self.assertRaises(ValueError):
+            transliteration('test', 'rus2ara')
