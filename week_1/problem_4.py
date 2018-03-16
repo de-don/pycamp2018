2d380fda5c53f05ef017c3ab15ca4c471d4bf96e
DenisDontsov <de-don@mail.ru>
2018-03-16 16:48:13 +0700

    Create more functions

--- week_1/problem_4.py
+++ week_1/problem_4.py
@@ -1,15 +1,16 @@
+from itertools import chain
+
+
 class Set:
     _items = None
 
     def __init__(self, iterable=None):
         items = list()
 
-        try:
+        if iterable:
             for i in iterable:
                 if i not in items:
                     items.append(i)
-        except TypeError:
-            pass
 
         self._items = items
 
@@ -22,5 +23,46 @@ class Set:
     def __iter__(self):
         return iter(self._items)
 
-    def __getitem__(self, item):
-        return self._items[item]
+    def __len__(self):
+        return len(self._items)
+
+    def __contains__(self, item):
+        return item in self._items
+
+    def isdisjoint(self, other):
+        for item in other:
+            if item in self:
+                return False
+        return True
+
+    def __le__(self, other):
+        """ self <= other """
+        for item in self:
+            if item not in other:
+                return False
+        return True
+
+    def issubset(self, other):
+        return self <= other
+
+    def __ge__(self, other):
+        """ self >= other """
+        for item in other:
+            if item not in self:
+                return False
+        return True
+
+    def issuperset(self, other):
+        return self >= other
+
+    def __eq__(self, other):
+        return self <= other <= self
+
+    def __or__(self, other):
+        return Set(chain(self, other))
+
+    def union(self, *args):
+        s = self
+        for other in args:
+            s = s | other
+        return s
--- week_1/problem_4_tests.py
+++ week_1/problem_4_tests.py
@@ -28,6 +28,38 @@ class TestSet(TestCase):
         s = Set(l)
         self.assertIterEqual(s, set(l))
 
+        with self.assertRaises(TypeError):
+            Set(1)
+
     def test_output(self):
         self.assertEqual(repr(Set([])), 'set()')
         self.assertEqual(repr(Set([1, 2, 3])), '{1, 2, 3}')
+
+    def test_comp(self):
+        s1 = Set((1, 2, 3))
+        s2 = Set((0, 2, 1, 3, 2))
+        s3 = Set((0, 2, 1, 3, 2, 1, 2, 3))
+
+        self.assertTrue(s1 <= s2)
+        self.assertTrue(s2 >= s1)
+        self.assertTrue(s1.issubset(s2))
+
+        self.assertTrue(s2 == s3)
+
+        self.assertFalse(s1 >= s2)
+        self.assertFalse(s2 <= s1)
+        self.assertFalse(s1.issuperset(s2))
+
+    def test_operations(self):
+        s1 = Set((1, 2, 3))
+        s2 = Set((3, 4, 5))
+        s3 = Set([7, 9])
+
+        self.assertIterEqual(s1 | s2, Set((1, 2, 3, 4, 5)))
+        self.assertIterEqual(s1.union(s2), Set((1, 2, 3, 4, 5)))
+
+        self.assertIterEqual(s1 | s2 | s3, Set((1, 2, 3, 4, 5, 7, 9)))
+        self.assertIterEqual(s1.union(s2, s3), Set((1, 2, 3, 4, 5, 7, 9)))
+
+        self.assertTrue(s1.isdisjoint(s3))
+        self.assertFalse(s1.isdisjoint(s2))
