966f98729049548d2500e9a735e9d7c09286df94
de-don <de-don@mail.ru>
2018-03-20 15:44:32 +0700

    Created protected class

--- problem_5/factory.py
+++ problem_5/factory.py
@@ -1,4 +1,10 @@
 from itertools import compress
+from collections import defaultdict
+
+
+class ProtectedError(PermissionError):
+    """ Error caused by actions with protected attributes. """
+    pass
 
 
 class BaseDict:
@@ -34,7 +40,7 @@ class BaseDict:
 
 
 class EditMixin:
-    """ Mixin which allow to edit a values by attribute """
+    """ Mixin which allow to edit a values by attribute. """
 
     def __setattr__(self, key, value):
         if key not in self.__dict__:
@@ -54,7 +60,7 @@ class AddMixin:
 
 
 class DelMixin:
-    """ Mixin which allow to delete only exist attribute """
+    """ Mixin which allow to delete only exist attribute. """
 
     def __delattr__(self, item):
         if item not in self.__dict__:
@@ -63,7 +69,60 @@ class DelMixin:
         del self.__dict__[item]
 
 
-def dict_factory(class_name, change=False, add=False, delete=False):
+class ProtectedMixin:
+    """ Mixin which allow to protect attributes.
+
+     Protected attribute is forbidden to change and delete.
+     If you try it, you get 'ProtectedError'.
+     """
+    __protected = []
+
+    def __init__(self, input_dict, protected):
+        self.__set_protected(protected)
+
+        cls = self.__class__
+        for key, value in input_dict.items():
+            # if value is dict, create new instance
+            if isinstance(value, dict):
+                value = cls(value, protected=self.__protected.get(key, []))
+            # save key:value
+            self.__dict__[key] = value
+
+    def __set_protected(self, protected):
+        """ Method to set process input list of attr names.
+
+        Method create dict of lists, where key - it is main-attribute name,
+        and key it is list of his sub-attributes or [''], if is simple
+        attribute. This dict set to self.protected for future using.
+
+        Args:
+             protected(List[str]): list of attr names, such as 'attr.subattr'
+        """
+
+        protected_filtered = defaultdict(list)
+        # filtering protected params
+        for item in protected:
+            parts = item.split(".")
+            if not parts:
+                continue
+            main_attr = parts[0]
+            sub_attr = ".".join(parts[1:])
+            protected_filtered[main_attr].append(sub_attr)
+        self.__protected = protected_filtered
+
+    def __setattr__(self, key, value):
+        if key not in self.__protected:
+            return super().__setattr__(key, value)
+        raise ProtectedError("this attribute is forbidden")
+
+    def __delattr__(self, item):
+        if item not in self.__protected:
+            return super().__delattr__(item)
+        raise ProtectedError("this attribute is forbidden")
+
+
+def dict_factory(class_name, change=False, add=False, delete=False,
+                 protected=False):
     """ Function to create class with selected permissions.
 
     Args:
@@ -71,12 +130,13 @@ def dict_factory(class_name, change=False, add=False, delete=False):
         change(bool): true, if have permission to change exists keys.
         add(bool): True, if have permission to add new keys.
         delete(bool): True, if have permission to delete exists keys.
+        protected(bool): True, if dict must have protected attrs.
 
     Returns:
         class: New class for storing dict with selected permissions.
     """
-    bases = [DelMixin, AddMixin, EditMixin, BaseDict]
-    flags = [delete, add, change, True]
+    bases = [ProtectedMixin, DelMixin, AddMixin, EditMixin, BaseDict]
+    flags = [protected, delete, add, change, True]
     bases = compress(bases, flags)
 
     return type(class_name, tuple(bases), {})
--- problem_5/factory_tests.py
+++ problem_5/factory_tests.py
@@ -1,10 +1,11 @@
-from problem_5.factory import dict_factory
+from problem_5.factory import dict_factory, ProtectedError
 from problem_5.main_tests import (
     SimpleDictTest,
     EditableDictTest,
     ExpandableDictTest,
     RemovableDictTest,
 )
+from unittest import TestCase
 
 # dictionaries for tests
 dict_1 = {'name': 'Denis', 'age': 22}
@@ -23,3 +24,68 @@ EditableDictTest.cls = dict_factory("EditableDict", change=True)
 ExpandableDictTest.cls = dict_factory("ExpandableDict", change=True, add=True)
 RemovableDictTest.cls = dict_factory("RemovableDict", change=True, add=True,
                                      delete=True)
+
+
+class ProtectedDictTest(TestCase):
+    permissions = dict(change=True, add=True, delete=True, protected=True)
+
+    def setUp(self):
+        self.cls = dict_factory("Protected", **self.permissions)
+
+    def test_init_and_read(self):
+        d = self.cls(dict_1, protected=['age'])
+        self.assertEqual(d.name, dict_1['name'])
+        self.assertEqual(d.age, dict_1['age'])
+        # try to get not exist attribute
+        with self.assertRaises(KeyError):
+            d.title
+
+        d = self.cls(dict_2, protected=['age', 'skills.fortran'])
+        self.assertEqual(d.skills.python, dict_2['skills']['python'])
+        # try to get not exist attribute
+        with self.assertRaises(KeyError):
+            d.skills.cpp
+
+    def test_change(self):
+        d1 = self.cls(dict_1, protected=['age'])
+        d2 = self.cls(dict_2, protected=['age', 'skills.fortran'])
+
+        # try to edit already exist attr
+        d1.name = 'Joan'
+        d2.skills.python = 'guru'
+        self.assertEqual(d1.name, 'Joan')
+        self.assertEqual(d2.skills.python, 'guru')
+
+        with self.assertRaises(ProtectedError):
+            d1.age = 23
+        with self.assertRaises(ProtectedError):
+            d2.skills.fortran = 'junior'
+        with self.assertRaises(ProtectedError):
+            d2.skills = 'junior'
+
+    def test_add(self):
+        d1 = self.cls(dict_1, protected=['age'])
+        d2 = self.cls(dict_2, protected=['age', 'skills.fortran'])
+
+        d1.title = 'example'
+        d2.skills.cpp = 'guru'
+        self.assertEqual(d1.title, 'example')
+        self.assertEqual(d2.skills.cpp, 'guru')
+
+    def test_delete(self):
+        d1 = self.cls(dict_1, protected=['age'])
+        d2 = self.cls(dict_2, protected=['age', 'skills.fortran'])
+
+        del d1.name
+        del d2.skills.python
+        with self.assertRaises(KeyError):
+            d1.name
+        with self.assertRaises(KeyError):
+            d2.skills.python
+
+        with self.assertRaises(ProtectedError):
+            del d1.age
+        with self.assertRaises(ProtectedError):
+            del d2.skills.fortran
+        with self.assertRaises(ProtectedError):
+            del d2.skills
