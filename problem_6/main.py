b179dba06d988225746ff886b41496bc9792d3df
DenisDontsov <de-don@mail.ru>
2018-03-21 12:08:07 +0700

    Refactoring

--- problem_6/main.py
+++ problem_6/main.py
@@ -1,14 +1,17 @@
-from collections import OrderedDict
-import operator
-from operator import itemgetter
 import datetime
+import operator
+from collections import OrderedDict
+from operator import itemgetter
+
 
 class NotSupported(ValueError):
+    """ Error raise which function not supported """
     pass
+
 
 class Series:
     def __init__(self, row, col_names):
-        row = list(map(self.detect_type, row))
+        row = list(map(self.detect_type, list(row)))
         col_names = list(col_names)
         if len(row) != len(col_names):
             ValueError("Len(row) != Len(col_names)")
@@ -17,31 +20,34 @@ class Series:
 
     @staticmethod
     def detect_type(item):
-        # date format: 2018-03-20 18:30:30
-        date_time = lambda x: datetime.datetime.strptime(str(x), '%Y-%m-%d %H:%M:%S')
-        date = lambda x: datetime.datetime.strptime(str(x), '%Y-%m-%d')
+        """ Detect type of item, int, float or date """
+        types = (
+            int,
+            float,
+            lambda x: datetime.datetime.strptime(str(x), '%Y-%m-%d %H:%M:%S'),
+            lambda x: datetime.datetime.strptime(str(x), '%Y-%m-%d')
+        )
 
-        types = (int, float, date_time, date)
+        # check each type
         for cur_type in types:
             try:
                 new_item = cur_type(item)
                 return new_item
             except (ValueError, TypeError):
                 pass
+        # if type not be found, leave str type
         return item
 
     def __getitem__(self, item):
         return self._items[item]
 
     def __str__(self):
-        s = []
-        for title, value in self._items.items():
-            s.append(f'{title}: {value}')
-        return "\n".join(s)
+        lines = (f'{title}: {value}' for title, value in self._items.items())
+        return "\n".join(lines)
 
     def __eq__(self, other):
         if not isinstance(other, self.__class__):
-            raise TypeError("Not same types to compare")
+            raise TypeError("Objects have different types")
         return self._items == other._items
 
     def __len__(self):
@@ -52,28 +58,30 @@ class Series:
 
 
 class Table:
-    def __init__(self, rows, col_names=None):
-        rows = list(rows)
-        self.rows_count = len(rows)
-        self.cols_count = len(rows[0]) if self.rows_count else 0
+    SUPPORTED_FUNCS = {
+        str: ['startswith', 'endswith'],
+        int: ['gt', 'lt', 'ge', 'le'],
+        datetime.datetime: ['gt', 'lt', 'ge', 'le'],
+    }
 
-        if not col_names:
-            self.col_names = [f'col_{i}' for i in range(self.cols_count)]
+    def __init__(self, rows, col_names):
         self.col_names = list(col_names)
+        self.cols_count = len(self.col_names)
 
         self.rows = [Series(row, self.col_names) for row in rows]
+        self.rows_count = len(self.rows)
 
     def __str__(self):
         if not self.rows_count:
             return "None"
 
-        s = []
+        lines = []
         for row_num, row in enumerate(self.rows):
-            s.append(f'{row_num}:')
-            for title in self.col_names:
-                value = row[title]
-                s.append(f'    {title}: {value}')
-        return "\n".join(s)
+            lines.append(f'{row_num}:')
+            lines.extend('    ' + line for line in str(row).splitlines())
+        return "\n".join(lines)
+
+    __repr__ = __str__
 
     @classmethod
     def from_csv(cls, file_path):
@@ -89,48 +97,49 @@ class Table:
 
     @staticmethod
     def split_key_filtering(key):
-        params = key.split("__")
-        if len(params) > 2:
-            raise KeyError("Sub-filtering not supported")
+        params = key.split("__", 1)
         if len(params) == 2:
             return params
         return params[0], None
 
     def filter(self, **kwargs):
-        supported_funcs = {
-            str: ['startswith', 'endswith'],
-            int: ['gt', 'lt', 'ge', 'le'],
-            datetime.datetime: ['gt', 'lt', 'ge', 'le'],
-        }
-
         data = self.copy()
         rows = data.rows
         for key, value in kwargs.items():
             key, func = self.split_key_filtering(key)
-            filter_func = None
 
             if func is None:
-                filter_func = lambda x: x[key] == value
-                rows = list(filter(filter_func, rows))
+                # if it is simple filtering without function
+                def filter_func(x):
+                    return x[key] == value
+
+                rows = filter(filter_func, rows)
                 continue
 
+            # if function exists, check type on supporting
             key_type = type(rows[0][key])
-            if not key_type in supported_funcs:
+            if key_type not in self.SUPPORTED_FUNCS:
                 raise NotSupported("Type not supported")
 
-            if func not in supported_funcs[key_type]:
-                raise NotSupported(f"Function {func} not supported"
-                                          f" for {key_type.__name__}")
+            # and check function on supporting
+            if func not in self.SUPPORTED_FUNCS[key_type]:
+                raise NotSupported(
+                    f"Function {func} not supported for {key_type.__name__}"
+                )
 
-            if getattr(key_type, func, None):
-                filter_func = lambda x: getattr(key_type, func)(x[key], value)
-            else:
-                filter_func = lambda x: getattr(operator, func)(x[key], value)
+            # if function not found for type, find it in operator
+            if not getattr(key_type, func, None):
+                key_type = operator
 
-            rows = list(filter(filter_func, rows))
+            # create function for filter
+            def filter_func(x):
+                return getattr(key_type, func)(x[key], value)
 
-        data.rows = rows
-        data.rows_count = len(rows)
+            rows = filter(filter_func, rows)
+
+        # create list from filters and save it's
+        data.rows = list(rows)
+        data.rows_count = len(data.rows)
         return data
 
     def count(self):
@@ -140,7 +149,7 @@ class Table:
         return sum(row[col_name] for row in self.rows)
 
     def avg(self, col_name):
-        return self.sum(col_name) / self.rows_count
+        return self.sum(col_name) / self.rows_count if self.rows_count else 0
 
     def unique(self, col_name):
         return set(row[col_name] for row in self.rows)
@@ -148,16 +157,13 @@ class Table:
     def __getitem__(self, item):
         if not isinstance(item, int):
             raise TypeError("Index must be integer.")
-
         return self.rows[item]
 
     def columns(self, *col_names):
-        new_rows = []
-        for row in self.rows:
-            new_row = []
-            for col_name in col_names:
-                new_row.append(row[col_name])
-            new_rows.append(new_row)
+        new_rows = (
+            (row[col_name] for col_name in col_names)
+            for row in self.rows
+        )
         return Table(new_rows, col_names)
 
     @property
@@ -167,19 +173,16 @@ class Table:
     def order_by(self, col_name, reversed=False):
         new_table = self.copy()
         new_table.rows.sort(key=itemgetter(col_name), reverse=reversed)
-
         return new_table
 
     def copy(self):
-        rows = (list(map(itemgetter(1), row)) for row in self.rows)
+        rows = (
+            map(itemgetter(1), row)
+            for row in self.rows
+        )
         return Table(rows, col_names=self.col_names)
 
     def __eq__(self, other):
         if not isinstance(other, self.__class__):
             raise TypeError("Not same types to compare")
         return self.rows == other.rows and self.col_names == self.col_names
-
-
-if __name__ == '__main__':
-    data = Table.from_csv('input.csv')
-    print(data)
