4b888c522d14ff97af0d93efaa0d39ef9c7e7c41
DenisDontsov <de-don@mail.ru>
2018-03-23 08:11:41 +0700

    Split complex tests on 2 tests

--- problem_5/factory_tests_fab.py
+++ problem_5/factory_tests_fab.py
@@ -4,8 +4,8 @@ from unittest import TestCase
 from problem_5.factory import dict_factory, ProtectedError
 
 # dictionaries for tests
-dict_1 = {'name': 'Denis', 'age': 22, 'lists': [1, 2, 3]}
-dict_2 = {
+dict_not_embedded = {'name': 'Denis', 'age': 22, 'lists': [1, 2, 3]}
+dict_embedded = {
     'name': 'Denis',
     'age': 22,
     'skills': {
@@ -24,112 +24,131 @@ class FactoryReadableTest(TestCase):
         self.cls = dict_factory("EditableRemovable", **self.permissions)
 
     def test_init_and_read_1(self):
-        d = self.cls(dict_1)
-        self.assertEqual(d.name, dict_1['name'])
-        self.assertEqual(d.age, dict_1['age'])
-        self.assertEqual(d.lists, dict_1['lists'])
+        d = self.cls(dict_not_embedded)
+        self.assertEqual(d.name, dict_not_embedded['name'])
+        self.assertEqual(d.lists, dict_not_embedded['lists'])
         # try to get not exist attribute
         with self.assertRaises(KeyError):
             d.title
 
     def test_init_and_read_2(self):
-        d = self.cls(dict_2)
-        self.assertEqual(d.skills.python, dict_2['skills']['python'])
+        d = self.cls(dict_embedded)
+        self.assertEqual(d.skills.python, dict_embedded['skills']['python'])
         # try to get not exist attribute
         with self.assertRaises(KeyError):
             d.skills.cpp
 
-    def test_change_allow(self):
+    def test_change_allow_1(self):
         if not self.permissions.get('change', False):
             return
-        d1 = self.cls(dict_1)
-        d2 = self.cls(dict_2)
+        d1 = self.cls(dict_not_embedded)
         d1.name = 'Joan'
-        d2.skills.python = 'guru'
         self.assertEqual(d1.name, 'Joan')
-        self.assertEqual(d2.skills.python, 'guru')
 
-    def test_change_denied(self):
+    def test_change_denied_2(self):
         if self.permissions.get('change', False):
             return
-        d1 = self.cls(dict_1)
-        d2 = self.cls(dict_2)
-        with self.assertRaises(PermissionError):
-            d1.name = 'Joan'
+        d2 = self.cls(dict_embedded)
         with self.assertRaises(PermissionError):
             d2.skills.python = 'guru'
 
-    def test_add_allow(self):
+    def test_add_allow_1(self):
         if not self.permissions.get('add', False):
             return
-        d1 = self.cls(dict_1)
-        d2 = self.cls(dict_2)
-
+        d1 = self.cls(dict_not_embedded)
         d1.title = 'example'
-        d2.skills.cpp = 'guru'
         self.assertEqual(d1.title, 'example')
+
+    def test_add_allow_2(self):
+        if not self.permissions.get('add', False):
+            return
+        d2 = self.cls(dict_embedded)
+        d2.skills.cpp = 'guru'
         self.assertEqual(d2.skills.cpp, 'guru')
 
-    def test_add_denied(self):
+    def test_add_denied_1(self):
         if self.permissions.get('add', False):
             return
-        d1 = self.cls(dict_1)
-        d2 = self.cls(dict_2)
+        d1 = self.cls(dict_not_embedded)
         with self.assertRaises(PermissionError):
             d1.title = 'example'
+
+    def test_add_denied_2(self):
+        if self.permissions.get('add', False):
+            return
+        d2 = self.cls(dict_embedded)
         with self.assertRaises(PermissionError):
             d2.skills.cpp = 'guru'
 
     def test_delete_allow(self):
         if not self.permissions.get('delete', False):
             return
-        d1 = self.cls(dict_1)
-        d2 = self.cls(dict_2)
-
+        d1 = self.cls(dict_not_embedded)
         del d1.name
-        del d2.skills.python
         with self.assertRaises(KeyError):
             d1.name
+
+    def test_delete_allow_2(self):
+        if not self.permissions.get('delete', False):
+            return
+        d2 = self.cls(dict_embedded)
+        del d2.skills.python
         with self.assertRaises(KeyError):
             d2.skills.python
 
-    def test_delete_denied(self):
+    def test_delete_denied_1(self):
         if self.permissions.get('delete', False):
             return
-        d1 = self.cls(dict_1)
-        d2 = self.cls(dict_2)
+        d1 = self.cls(dict_not_embedded)
         with self.assertRaises(PermissionError):
             del d1.name
+
+    def test_delete_denied_2(self):
+        if self.permissions.get('delete', False):
+            return
+        d2 = self.cls(dict_embedded)
         with self.assertRaises(PermissionError):
             del d2.skills.python
 
-    def test_delete_protected(self):
+    def test_delete_protected_1(self):
         if not self.permissions.get('delete', False):
             return
         if not self.permissions.get('protect', False):
             return
 
-        d1 = self.cls(dict_1, protected=['age'])
-        d2 = self.cls(dict_2, protected=['age', 'skills.fortran'])
-
+        d1 = self.cls(dict_not_embedded, protected=['age'])
         with self.assertRaises(ProtectedError):
             del d1.age
+
+    def test_delete_protected_2(self):
+        if not self.permissions.get('delete', False):
+            return
+        if not self.permissions.get('protect', False):
+            return
+        d2 = self.cls(dict_embedded, protected=['age', 'skills.fortran'])
         with self.assertRaises(ProtectedError):
             del d2.skills.fortran
         with self.assertRaises(ProtectedError):
             del d2.skills
 
-    def test_change_protected(self):
+    def test_change_protected_1(self):
         if not self.permissions.get('change', False):
             return
         if not self.permissions.get('protect', False):
             return
 
-        d1 = self.cls(dict_1, protected=['age'])
-        d2 = self.cls(dict_2, protected=['age', 'skills.fortran'])
-
+        d1 = self.cls(dict_not_embedded, protected=['age'])
         with self.assertRaises(ProtectedError):
             d1.age = 23
+
+    def test_change_protected_2(self):
+        if not self.permissions.get('change', False):
+            return
+        if not self.permissions.get('protect', False):
+            return
+
+        d2 = self.cls(dict_embedded, protected=['age', 'skills.fortran'])
+
         with self.assertRaises(ProtectedError):
             d2.skills.fortran = 'junior'
         with self.assertRaises(ProtectedError):
