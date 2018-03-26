56091cd3512d419c8eaaf3baf012ab0c469ed396
DenisDontsov <de-don@mail.ru>
2018-03-26 14:19:18 +0700

    complete logic

--- task_7/main.py
+++ task_7/main.py
@@ -2,6 +2,11 @@ import hashlib
 from collections import defaultdict
 from pathlib import Path
 
+import click
+
+DELETE_TEXT = 'Enter number of files, separated by space, which you want to ' \
+              'remove. Enter 0, for skip it.'
+CONFIRM_TEXT = 'Delete?'
 
 def get_file_hash(file_path):
     """ This function returns the SHA-1 hash of the file passed into it"""
@@ -39,18 +44,55 @@ def recursion_finder(path):
             yield h
 
 
-if __name__ == '__main__':
-    dir_path = Path('../')
+@click.command()
+@click.argument('path_to_dir', type=click.Path(exists=True))
+@click.option('-d', 'delete', is_flag=True, help="Show delete dialog?")
+def find_copies(path_to_dir, delete):
+    dir_path = Path(path_to_dir)
 
-    items = recursion_finder(dir_path)
+    files_iter = recursion_finder(dir_path)
     storage = defaultdict(list)
 
-    for file_hash, file_path in items:
+    # find all files and save his path and hash to dict.
+    for file_hash, file_path in files_iter:
         storage[file_hash].append(file_path)
 
-    replies = list(filter(lambda x: len(x[1]) > 1, storage.items()))
+    # filter copies and unique files:
+    copies = filter(lambda x: len(x[1]) > 1, storage.items())
 
-    for sha1, copies in replies:
-        print(f'Next files has sha1={sha1}:')
-        for item in copies:
-            print(f'    {item}')
+    for sha1, files in copies:
+        click.echo(f'Next files has sha1={sha1}:')
+        for item_num, item in enumerate(files, 1):
+            click.echo(f'    {item_num}) {item}.')
+
+        if delete:
+            while True:
+                delete_nums = click.prompt(DELETE_TEXT, type=str, default=0)
+                if delete_nums in [0, '0']:
+                    break
+                try:
+                    delete_nums = delete_nums.split(" ")
+                    delete_nums = map(str.strip, delete_nums)
+                    delete_nums = map(int, delete_nums)
+                except Exception as e:
+                    print(e)
+                    continue
+
+                click.echo(f'Next files will be deleted:')
+                for num in delete_nums:
+                    item = files[num-1]
+                    click.echo(f'    {item}')
+
+                if click.confirm(CONFIRM_TEXT):
+                    click.echo('Delete success!')
+                    # Todo: delete
+                else:
+                    click.echo('Delete aborted!')
+
+                break
+        click.echo('='*20)
+
+
+
+if __name__ == '__main__':
+    find_copies()
