99b23803d4f262ffa8dca12aba4bb0141d341795
DenisDontsov <de-don@mail.ru>
2018-03-26 14:44:37 +0700

    some edits

--- task_7/main.py
+++ task_7/main.py
@@ -4,12 +4,20 @@ from pathlib import Path
 
 import click
 
-DELETE_TEXT = 'Enter number of files, separated by space, which you want to ' \
-              'remove. Enter 0, for skip it.'
-CONFIRM_TEXT = 'Delete?'
+TEXTS = {
+    'identical_files': 'Next files are identical:',
+    'delete': 'Enter number of files, separated by space, which you'
+              ' want to remove. Enter 0, for skip it.',
+    'confirm': 'Really delete?',
+    'delete_list': 'Next files will be deleted:',
+    'delete_success': 'Delete success!',
+    'delete_aborted': 'Delete aborted!',
+    'help_delete': 'Show delete dialog?',
+}
+
 
 def get_file_hash(file_path):
-    """ This function returns the SHA-1 hash of the file passed into it"""
+    """ This function returns the SHA-1 hash of the file """
     h = hashlib.sha1()
 
     # open file for reading in binary mode
@@ -25,15 +33,16 @@ def get_file_hash(file_path):
     return h.hexdigest()
 
 
-def file_data(item: Path):
+def file_data(item):
+    """ Function to get file sha1, if file not empty, else None"""
     size = item.stat().st_size
     if size == 0:
         return None
-
     return get_file_hash(item), item
 
 
 def recursion_finder(path):
+    """ Recursion generator to find all not_empty files and get his sha1 """
     for item in path.iterdir():
         if item.is_dir():
             yield from recursion_finder(item)
@@ -44,9 +53,21 @@ def recursion_finder(path):
             yield h
 
 
+def process_str_of_nums(numbers, range_num=None):
+    for num in numbers.split(" "):
+        num = int(num.strip())
+
+        if not range_num:
+            yield num
+            continue
+        if range_num[0] > num  or num > range_num[1]:
+            raise IndexError(f'Index {num} out of range {range_num}')
+        yield num
+
+
 @click.command()
 @click.argument('path_to_dir', type=click.Path(exists=True))
-@click.option('-d', 'delete', is_flag=True, help="Show delete dialog?")
+@click.option('-d', 'delete', is_flag=True, help=TEXTS['help_delete'])
 def find_copies(path_to_dir, delete):
     dir_path = Path(path_to_dir)
 
@@ -61,37 +82,40 @@ def find_copies(path_to_dir, delete):
     copies = filter(lambda x: len(x[1]) > 1, storage.items())
 
     for sha1, files in copies:
-        click.echo(f'Next files has sha1={sha1}:')
+        click.echo(TEXTS['identical_files'])
         for item_num, item in enumerate(files, 1):
             click.echo(f'    {item_num}) {item}.')
 
-        if delete:
-            while True:
-                delete_nums = click.prompt(DELETE_TEXT, type=str, default=0)
-                if delete_nums in [0, '0']:
-                    break
-                try:
-                    delete_nums = delete_nums.split(" ")
-                    delete_nums = map(str.strip, delete_nums)
-                    delete_nums = map(int, delete_nums)
-                except Exception as e:
-                    print(e)
-                    continue
+        if not delete:
+            continue
 
-                click.echo(f'Next files will be deleted:')
-                for num in delete_nums:
-                    item = files[num-1]
-                    click.echo(f'    {item}')
+        while True:
+            # wait user input
+            users_input = click.prompt(TEXTS['delete'], default='0')
+            try:
+                nums = process_str_of_nums(users_input, [0, len(files)])
+                nums = list(nums)
+            except Exception as exp:
+                click.echo(f'Error: {exp}')
+                continue
 
-                if click.confirm(CONFIRM_TEXT):
-                    click.echo('Delete success!')
-                    # Todo: delete
-                else:
-                    click.echo('Delete aborted!')
-
+            if 0 in nums:
                 break
-        click.echo('='*20)
 
+            # show files to delete
+            click.echo(TEXTS['delete_list'])
+            for num in nums:
+                item = files[num - 1]
+                click.echo(f'    {item}')
+
+            if click.confirm(TEXTS['confirm']):
+                click.echo(TEXTS['delete_success'])
+                # Todo: delete
+            else:
+                click.echo(TEXTS['delete_aborted'])
+
+            break
+        click.echo('=' * 20)
 
 
 if __name__ == '__main__':
