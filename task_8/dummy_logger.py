e93b77a6e2c4eaa8520d99c80b7cbcc55ef96e83
de-don <de-don@mail.ru>
2018-03-27 14:36:18 +0700

    print time and edit fmt

--- task_8/dummy_logger.py
+++ task_8/dummy_logger.py
@@ -3,21 +3,13 @@ from datetime import datetime
 
 
 def dummy_logger(func):
-    fmt = 'Called {name}(args={args}, kwargs={kwargs}) called at {time}'
+    fmt = 'Called {name}(args={args}, kwargs={kwargs}) at {time}'
 
     @wraps(func)
     def _wrap(*args, **kwargs):
-        time = datetime.now().strftime("")
+        time = datetime.now().strftime('%H:%M')
         name = func.__name__
         print(fmt.format(name=name, args=args, kwargs=kwargs, time=time))
         return func(*args, **kwargs)
 
     return _wrap
-
-
-@dummy_logger
-def sqrt(x):
-    return x**.5
-
-
-print(sqrt(x=10))
