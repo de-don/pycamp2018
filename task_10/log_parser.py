b66b8b42f8eff1afad9d3dca65f80de1779ac347
DenisDontsov <de-don@mail.ru>
2018-03-29 11:18:19 +0700

    getting url patterns and params

--- task_10/log_parser.py
+++ task_10/log_parser.py
@@ -1,5 +1,6 @@
 import json
 import re
+from operator import itemgetter
 from pprint import pprint
 
 data = None
@@ -12,15 +13,41 @@ pattern = r'(?P<date>\d{4}\-\d{2}\-\d{2} \d{2}:\d{2}:\d{2},\d{1,4}) ' \
           r'Query:\n(?P<query>{.*?})\nHeaders:\n(?P<headers>{.*?})'
 
 
+def get_url_pattern(url):
+    matches = re.finditer(r'([^/]+)/(\d+)/', url)
+    params = {}
+    for match in matches:
+        name, value = match.groups()
+        params[name] = value
+
+    pattern = re.sub(r'([^/]+)/(\d+)/', r'\1/<\1-id>/', url)
+    return pattern, params
+
+
+def get_url_patterns(url_patterns):
+    dictionary = {}
+    for url in url_patterns:
+        url = url.split("/")[1:-1]
+        curr_dict = dictionary
+        for i, part in enumerate(url):
+            if part not in curr_dict:
+                curr_dict[part] = {}
+            curr_dict = curr_dict[part]
+    return dictionary
+
+
 def match_to_dict(matchobj):
     log = matchobj.groupdict()
     log['headers'] = json.loads(log['headers'])
     log['query'] = json.loads(log['query'])
+    log['url_pattern'], log['url_params'] = get_url_pattern(log['url'])
     return log
 
 
 matches = re.finditer(pattern, data, flags=re.DOTALL)
-
 logs = [match_to_dict(match) for match in matches]
-
 pprint(logs)
+
+input("press key...")
+urls_dict = get_url_patterns(map(itemgetter('url_pattern'), logs))
+pprint(urls_dict)
