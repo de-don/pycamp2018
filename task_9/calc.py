3d823f28b3a7abd83f670b68e1ebb88c7c124af7
DenisDontsov <de-don@mail.ru>
2018-03-29 08:29:22 +0700

    Add delete spaces, checking other symbols.

--- task_9/calc.py
+++ task_9/calc.py
@@ -1,6 +1,14 @@
 import re
+from operator import mul, truediv, add, sub
 
 number = r'-?\d+(?:\.\d+)?'
+
+operations = {
+    '*': mul,
+    '/': truediv,
+    '+': add,
+    '-': sub
+}
 
 
 def brackets(q):
@@ -8,49 +16,41 @@ def brackets(q):
     return q[0] + str(calc(q[1])) + q[2]
 
 
+def arithm_oper(q):
+    q = q.groups()
+    result = operations[q[1]](float(q[0]), float(q[2]))
+    return str(result)
+
+
 def calc(expr):
-    q = re.search(r'(.*?)\((.*)\)(.*?)', expr)
-    if q:
-        result = brackets(q)
-        expr = re.sub(r'(.*?)\((.*)\)(.*?)', str(result), expr)
-
-        return calc(expr)
-
-    # тут выражение уже без скобочек
-
-    def mul_div(q):
-        q = q.groups()
-        i1 = q[0]
-        i2 = q[2]
-        op = q[1]
-        if op == "*":
-            result = float(i1) * float(i2)
-        if op == "/":
-            result = float(i1) / float(i2)
-        return str(result)
-
+    # brackets
     while True:
-        new_expr = re.sub(r'(' + number + r')(\*|\/)(' + number + r')', mul_div, expr)
+        new_expr = re.sub(r'(.*?)\((.*)\)(.*?)', brackets, expr)
         if new_expr == expr:
             break
         expr = new_expr
 
-
-    def plus_minus(q):
-        q = q.groups()
-        i1 = q[0]
-        i2 = q[2]
-        op = q[1]
-        if op == "+":
-            result = float(i1) + float(i2)
-        if op == "-":
-            result = float(i1) - float(i2)
-        return str(result)
-
-    while True:
-        new_expr = re.sub(r'(' + number + r')(\+|-)(' + number + r')', plus_minus, expr)
-        if new_expr == expr:
-            break
-        expr = new_expr
+    for oper in (r'\*|\/', r'\+|-'):
+        # mul and truediv
+        while True:
+            new_expr = re.sub(
+                r'({num})({op})({num})'.format(num=number, op=oper),
+                arithm_oper,
+                expr
+            )
+            if new_expr == expr:
+                break
+            expr = new_expr
 
     return float(expr)
+
+
+def calculate(expr):
+    # delete spaces
+    expr = re.sub(r'\s', '', expr)
+    # check other symbols:
+    other = re.findall(r'[^\*\/\-\+0-9\(\)\.]', expr)
+    if other:
+        raise ValueError(f"Find not arithmetic symbols: {other}")
+
+    return calc(expr)
